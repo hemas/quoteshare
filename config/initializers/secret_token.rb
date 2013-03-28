8154cbc6a4e049fa4e9b38802327c742da732890
hemas <hemapappu@gmail.com>
2013-03-28 09:46:17 -0500

    Created quoteshare app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quoteshare::Application.config.secret_token = '2665b0fbdffdb29ca130e89b0a28b218e99e35bb116bc618b3fe75619391411ee14a8f9ccb22d09fe39a5afb78320cd8e6ebf482fef1cc4deb2fdca278f5a00a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Quoteshare::Application.config.session_store :cookie_store, key: '_quoteshare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Quoteshare::Application.config.session_store :active_record_store
